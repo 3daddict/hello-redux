49dded5ef6ee3e04c15a831f6edf26b52866b676
Michael <print@3daddict.com>
2019-02-04 21:44:57 -0800

    refactoring app state to reducer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React, { Component } from "react";
-import HelloWorld from "./HelloWorld";
-
-class App extends Component {
-  state = {
-    tech: "React"
-  };
-  render() {
-    return <HelloWorld tech={this.state.tech} />;
-  }
+   import HelloWorld from "./HelloWorld";
+    import { createStore } from "redux";
+    const store = createStore(reducer);
+    class App extends Component {
+    render() {
+      return <HelloWorld tech={this.state.tech}/>
+    }
 }
-
-export default App;
+   export default App;
